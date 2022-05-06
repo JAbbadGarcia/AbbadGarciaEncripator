2fc7d0accb21b6c84d880f40b746fd8edb457ec9
JAbbadGarcia <99227496+JAbbadGarcia@users.noreply.github.com>
2022-05-06 17:21:37 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+const encryptfn = () => {
+  var msg = document.getElementById("text-area-input").value.toLowerCase();
+  if (msg !== "") {
+    msgFilter = msg.replace(/[^a-zA-Z 0-9.]+/g, " ");
+    var textEncrypt = msgFilter
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .replaceAll("e", "enter")
+      .replaceAll("i", "imes")
+      .replaceAll("o", "ober")
+      .replaceAll("a", "ai")
+      .replaceAll("u", "ufat");
+    document.getElementById("text-area-output").innerHTML = textEncrypt;
+    document.getElementById("no-message").style = "display:none";
+    document.getElementById("enter-message").style = "display:none";
+    document.getElementById("copy").style = "display:inline";
+    document.getElementById("text-area-input").value = "";
+  } else {
+    alert("Ingresa texto para continuar");
+    document.getElementById("text-area-input").focus();
+    document.getElementById("copy").style = "display:none";
+    document.getElementById("no-message").style = "display:inline-block";
+    document.getElementById("enter-message").style = "display:inline-block";
+    document.getElementById("text-area-output").value = "";
+  }
+};
+const decryptfn = () => {
+  var msg = document.getElementById("text-area-input").value.toLowerCase();
+  if (msg !== "") {
+    msgFilter = msg.replace(/[^a-zA-Z 0-9.]+/g, " ");
+    var textDecrypt = msgFilter
+      .toLowerCase()
+      .replaceAll("enter", "e")
+      .replaceAll("imes", "i")
+      .replaceAll("ober", "o")
+      .replaceAll("ai", "a")
+      .replaceAll("ufat", "u");
+    document.getElementById("text-area-output").innerHTML = textDecrypt;
+    document.getElementById("no-message").style = "display:none";
+    document.getElementById("enter-message").style = "display:none";
+    document.getElementById("copy").style = "display:inline";
+    document.getElementById("text-area-input").value = "";
+  } else {
+    alert("Ingresa texto para continuar");
+    document.getElementById("text-area-input").focus();
+    document.getElementById("copy").style = "display:none";
+    document.getElementById("no-message").style = "display:inline-block";
+    document.getElementById("enter-message").style = "display:inline-block";
+    document.getElementById("text-area-output").value = "";
+  }
+};
+const copytext = () => {
+  var copy = document.getElementById("text-area-output");
+  navigator.clipboard.writeText(copy.innerHTML);
+};
